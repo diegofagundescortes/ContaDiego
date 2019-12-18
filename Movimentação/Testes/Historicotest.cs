6cc8cda2319a7fa70cb7fe3e797bfbc19235dd4e
diego-fagundes <diego.cortes@escolarmanager.com.br>
2019-12-18 10:39:36 -0300

    Criação windows form

---
+++ Movimentação/Testes/Historicotest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using Xunit;
+
+namespace ContaDiego
+{
+
+    class Historicotest
+    {
+        Historico _historico;
+        [Test]
+        public void TesteCriacaoHistorico()
+        {
+            _historico = new Historico(1, new Saldo(15.14),new Saldo(24.15), new DateTime(2019, 18, 01));
+            Assert.Equals(24.15, _historico.saldoAnterior.saldo);
+            Assert.Equals(15.14, _historico.saldoAtual.saldo);
+        }
+
+        [Test]
+        public void TesteRespostaHistorico()
+        {
+            Assert.Equals(2019, _historico.data.Year);
+            Assert.Equals(-1, _historico.saldoAtual.idSaldo);
+        }
+
+    }
+}
